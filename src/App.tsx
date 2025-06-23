899b6a32b648bf944b12a8a211c3168382c29ce8
matgdev <math.minp@gmail.com>
2025-06-23 10:55:09 -0300

    chore: apply formatting and ESlint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { Slide } from "./Slide"
-import { SlideLayout } from "./SlideLayout"
+import { Slide } from "./Slide";
+import { SlideLayout } from "./SlideLayout";
 
 function App() {
-  return (
-    <div className="w-screen h-screen">
-      <SlideLayout>
-        <Slide>
-          <SlideLayout>
-            <Slide>SS1</Slide>
-            <Slide>SS2</Slide>
-          </SlideLayout>
-        </Slide>
-        <Slide>S2</Slide>
-        <Slide>S3</Slide>
-        <Slide>S4</Slide>
-      </SlideLayout>
-    </div>
-    
-  )
+    return (
+        <div className="h-screen w-screen">
+            <SlideLayout>
+                <Slide>
+                    <SlideLayout>
+                        <Slide>SS1</Slide>
+                        <Slide>SS2</Slide>
+                    </SlideLayout>
+                </Slide>
+                <Slide>S2</Slide>
+                <Slide>S3</Slide>
+                <Slide>S4</Slide>
+            </SlideLayout>
+        </div>
+    );
 }
 
-export default App
+export default App;
