6cbb039124ac58b841bd66c1aebf436745b82a18
matgdev <math.minp@gmail.com>
2025-06-23 10:54:35 -0300

    fix: resolve tailwind import issue

--- src/Slide.tsx
+++ src/Slide.tsx
@@ -1,16 +1,11 @@
-import "./theme.css"
+import type React from "react";
 
-export function Slide(props: any) {
-
-
-    let className = "bg-light-primary border border-light-border dark:border-dark-border dark:bg-dark-primary dark:text-bg-dark-secondary rounded p-4 w-full h-full";
+export function Slide(props: React.HTMLAttributes<HTMLDivElement>) {
+    let className =
+        "bg-light-primary border border-light-border dark:border-dark-border dark:bg-dark-primary dark:text-bg-dark-secondary rounded p-4 w-full h-full";
     let children = false;
     if (props.className) className = className.concat(" ", props.className);
     if (props.children) children = true;
 
-    return (
-        <div className={className}>
-            {children ? props.children : ""}
-        </div>
-    );
+    return <div className={className}>{children ? props.children : ""}</div>;
 }
--- src/SlideLayout.tsx
+++ src/SlideLayout.tsx
@@ -1,14 +1,12 @@
-import React, { useState, type ReactNode, useRef, type ReactElement, type Ref, useLayoutEffect, useEffect } from "react";
+import React, { useState, useRef, type ReactElement } from "react";
 import "./SlideLayout.css";
-import "./theme.css"
 
 interface AnimState {
-    exitAnim: "to-top" | "to-bottom" | null,
-    enterAnim: "from-top" | "from-bottom" | null
+    exitAnim: "to-top" | "to-bottom" | null;
+    enterAnim: "from-top" | "from-bottom" | null;
 }
 
 export function SlideLayout({ children }: { children?: Array<ReactElement> }) {
-
     const [activeSlideIdx, setActiveSlideIdx] = useState(0);
     const [animationSet, setAnimationSet] = useState<AnimState | null>(null);
     const throttleScroll = useRef(false);
@@ -18,34 +16,35 @@ export function SlideLayout({ children }: { children?: Array<ReactElement> }) {
     const transitionCount = useRef(0);
 
     const handleClickNavigation = (event: React.MouseEvent<HTMLDivElement>) => {
-
         if (isAnimating.current) return;
 
         const target = event.target as HTMLElement;
         const navigationIndex = Number(target.dataset.id);
-    
+
         isAnimating.current = true;
         scrollCount.current = navigationIndex - activeSlideIdx;
-        setAnimationSet((curr) => {
+        setAnimationSet(() => {
             return {
-                exitAnim: (navigationIndex < activeSlideIdx ? "to-bottom" : "to-top"),
-                enterAnim: null
-            }
-        })
-    }
+                exitAnim: navigationIndex < activeSlideIdx ? "to-bottom" : "to-top",
+                enterAnim: null,
+            };
+        });
+    };
 
     const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
         event.stopPropagation();
         touchStart.current = event.touches[0].clientY;
-    }
+    };
 
-    const handleWheel = (event: React.WheelEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => { 
+    const handleWheel = (
+        event: React.WheelEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
+    ) => {
         event.stopPropagation();
         let deltaY = 0;
-        
-        if ("touches" in event){
+
+        if ("touches" in event) {
             deltaY = touchStart.current - event.changedTouches[0].clientY;
-        }else{
+        } else {
             deltaY = event.deltaY;
         }
 
@@ -54,48 +53,52 @@ export function SlideLayout({ children }: { children?: Array<ReactElement> }) {
 
         scrollCount.current += deltaY < 0 ? -1 : 1;
 
-        if (!   (scrollCount.current === 0
-                || (scrollCount.current > 0 && (activeSlideIdx >= (children?.length ? children.length - 1 : 0)))
-                || (scrollCount.current < 0 && activeSlideIdx <= 0)
-                )) {
-
+        if (
+            !(
+                scrollCount.current === 0 ||
+                (scrollCount.current > 0 &&
+                    activeSlideIdx >= (children?.length ? children.length - 1 : 0)) ||
+                (scrollCount.current < 0 && activeSlideIdx <= 0)
+            )
+        ) {
             if (isAnimating.current == false) {
                 isAnimating.current = true;
-                setAnimationSet((curr) => {
+                setAnimationSet(() => {
                     return {
-                        exitAnim: (deltaY < 0 ? "to-bottom" : "to-top"),
-                        enterAnim: null
-                    }
+                        exitAnim: deltaY < 0 ? "to-bottom" : "to-top",
+                        enterAnim: null,
+                    };
                 });
             }
-
-        } else{
+        } else {
             scrollCount.current -= deltaY < 0 ? -1 : 1;
         }
 
-        setTimeout(() => throttleScroll.current = false, 50);
-    }
+        setTimeout(() => (throttleScroll.current = false), 50);
+    };
 
     const handleTransitionEnd = (event: React.TransitionEvent) => {
         event.stopPropagation();
         transitionCount.current += 1;
         if (transitionCount.current === 2) {
-            setActiveSlideIdx((curr: number) => clamp(curr + scrollCount.current, 0, children?.length ? children.length - 1 : 0));
-            setAnimationSet(curr => {
+            setActiveSlideIdx((curr: number) =>
+                clamp(curr + scrollCount.current, 0, children?.length ? children.length - 1 : 0)
+            );
+            setAnimationSet((curr) => {
                 return {
                     enterAnim: curr?.exitAnim === "to-top" ? "from-bottom" : "from-top",
-                    exitAnim: null
-                }
+                    exitAnim: null,
+                };
             });
             transitionCount.current = 0;
         }
-    }
+    };
 
     const handleAnimationEnd = (event: React.AnimationEvent) => {
         event.stopPropagation();
         isAnimating.current = false;
         scrollCount.current = 0;
-    }
+    };
 
     const classNames = [];
 
@@ -112,33 +115,39 @@ export function SlideLayout({ children }: { children?: Array<ReactElement> }) {
         }
     }
 
+    const currentSlide = (
+        <div
+            key={activeSlideIdx}
+            className={
+                "h-full w-full transition-[opacity_transform] duration-300 ease-in " +
+                classNames.join(" ")
+            }
+            onTransitionEnd={handleTransitionEnd}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            {children?.filter((x, id) => x && id === activeSlideIdx)}
+        </div>
+    );
 
-    const currentSlide = <div
-        key={activeSlideIdx}
-        className={"w-full h-full transition-[opacity_transform] duration-300 ease-in " + classNames.join(" ")}
-        onTransitionEnd={handleTransitionEnd}
-        onAnimationEnd={handleAnimationEnd}>
-        {children?.filter((x, id) => x && id === activeSlideIdx)}
-    </div>
-
-    const slideBar = children?.map((c, idx) => <div 
-                                                    key={idx}
-                                                    data-id={idx}
-                                                    className={`h-[5px] rounded border border-light-border dark:border-dark-border flex-1 ${idx === activeSlideIdx ? "bg-emphasis " : " cursor-pointer " }`}
-                                                    onClick={idx === activeSlideIdx ? undefined : handleClickNavigation}
-                                                    ></div>);
+    const slideBar = children?.map((_c, idx) => (
+        <div
+            key={idx}
+            data-id={idx}
+            className={`h-[5px] flex-1 rounded border border-light-border dark:border-dark-border ${idx === activeSlideIdx ? "bg-emphasis" : "cursor-pointer"}`}
+            onClick={idx === activeSlideIdx ? undefined : handleClickNavigation}
+        ></div>
+    ));
     return (
-        <div className="w-full h-full bg-linear-to-br from-zinc-300 to-neutral-300 text-neutral-950 dark:from-gray-800 dark:to-neutral-800 dark:text-gray-300 p-4 flex flex-col items-stretch justify-stretch overflow-hidden touch-pan-y" 
+        <div
+            className="flex h-full w-full touch-pan-y flex-col items-stretch justify-stretch overflow-hidden bg-linear-to-br from-zinc-300 to-neutral-300 p-4 text-neutral-950 dark:from-gray-800 dark:to-neutral-800 dark:text-gray-300"
             onWheel={handleWheel}
             onTouchStart={handleTouchStart}
             onTouchEnd={handleWheel}
-            >
-                <div className="w-full flex justify-center gap-1 mb-2 self-center">
-                    {slideBar}
-                </div>
+        >
+            <div className="mb-2 flex w-full justify-center gap-1 self-center">{slideBar}</div>
             {currentSlide}
         </div>
-    )
+    );
 }
 
 function clamp(num: number, left: number, right: number) {
